ffd182cf3ca7e4d11500f361d95aa819923e5d1b
Joanna Buchała <joanna.b2@op.pl>
2018-08-01 23:44:15 +0200

    simpler indeed

--- index.js
+++ index.js
@@ -4,27 +4,18 @@ const formatDate = (timeInSeconds) => {
 	const seconds = Math.floor(timeInSeconds % 3600 % 60);
 
 	let time;
-	if (hours < 1 && minutes < 1) {
-		time = `${seconds}s`;
-	} else if (timeInSeconds == undefined) {
-		time = `0s`;
-	} else if (hours < 1 && (minutes > 0) && (seconds > 0)) {
-		time = `${minutes}m ${seconds}s`;
-	} else if (hours < 1 && (minutes > 1) && (seconds > 0)) {
-		time = `${minutes}m ${seconds}s`;
-	} else if (hours < 1 && seconds % 60 === 0) {
-		time = `${minutes}m`;
-	} else if (hours > 0 && (minutes % 60 !== 0) && (seconds % 60 !== 0)) {
-		time = `${hours}h ${minutes}m ${seconds}s`;
-	} else if (hours > 1 && (minutes % 60 !== 0) && (seconds % 60 !== 0)) {
-		time = `${hours}h ${minutes}m ${seconds}s`;
-	} else if (hours > 0 && (minutes === 0) && (seconds === 0)) {
-		time = `${hours}h`;
-	} else if (hours > 0 && (minutes > 0) && (seconds === 0)) {
-		time = `${hours}h ${minutes}m`;
-	} else if (hours > 0 && (minutes === 0) && (seconds > 0)) {
-		time = `${hours}h ${seconds}s`;
-	}
+
+	(hours < 1 && minutes < 1) ? time = `${seconds}s` : false;
+	(timeInSeconds == undefined) ? time = `0s` : false;
+	(hours < 1 && (minutes > 0) && (seconds > 0)) ? time = `${minutes}m ${seconds}s` : false;
+	(hours < 1 && (minutes > 1) && (seconds > 0)) ? time = `${minutes}m ${seconds}s` : false;
+	(hours < 1 && seconds % 60 === 0) ? time = `${minutes}m` : false;
+	(hours > 0 && (minutes % 60 !== 0) && (seconds % 60 !== 0)) ? time = `${hours}h ${minutes}m ${seconds}s` : false;
+	(hours > 1 && (minutes % 60 !== 0) && (seconds % 60 !== 0)) ? time = `${hours}h ${minutes}m ${seconds}s` : false;
+	(hours > 0 && (minutes === 0) && (seconds === 0)) ? time = `${hours}h` : false;
+	(hours > 0 && (minutes > 0) && (seconds === 0)) ? time = `${hours}h ${minutes}m` : false;
+	(hours > 0 && (minutes === 0) && (seconds > 0)) ? time = `${hours}h ${seconds}s` : false;
+
 	return `${time}`;
 }
 
